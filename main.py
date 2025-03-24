2675525e314dd2d0847c7c7e38ed380464a97205
Sapster <Mhlophenqo16@gmail.com>
2025-03-24 16:43:47 +0200

    Initialize Turtle screen and position

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter
+import turtle
+from turtle import *
+from tkinter import ttk
+
+TT = turtle.Turtle()
+
+screen = TT.getscreen()
+
+TT.penup()
+TT.goto(0,-250)
+TT.left(90)
+
+
+
+screen.mainloop()
+
+
+
+
